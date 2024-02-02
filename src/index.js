bb925b9b008ccef51239d66822057afd1048a0c9
JuliaDosSantos <santos.ju1995@gmail.com>
2024-02-01 23:40:41 -0300

    Cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
 server.use(express.json());
-
+server.use(cors());
 
 const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes);
 
+
 server.listen(3333, () => {
-  console.log('API ONLINE');
-});
+    console.log('API online');
+})
